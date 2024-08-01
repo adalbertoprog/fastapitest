7c102c34c43c4972b7407e6052a9b8aeb49e7761
Gabriel Vieira <72738617+gabrielgits@users.noreply.github.com>
2024-08-01 14:06:29 +0100

    category

--- app/data.py
+++ app/data.py
@@ -1,6 +1,14 @@
+from app.category_model import CategoryModel
+
 my_articles = [
     {"title": "title of article 1", "content": "content of article 1", "id": 1},
     {"title": "title of article 2", "content": "content of article 2", "id": 2}
+]
+
+listCategory = [
+    CategoryModel(id=1, name="category 1"),
+    CategoryModel(id=2, name="category 2"),
+    CategoryModel(id=3, name="category 3"),
 ]
 
 def find_article(id):
--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from app.author import router as author_router
+from app.category import router as category_router
 
 
 app = FastAPI()
 
-app.include_router(author_router)
+app.include_router(category_router)
 
 @app.get("/")
 def read_root():
